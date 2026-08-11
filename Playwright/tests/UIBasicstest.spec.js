72805922d9582b39b3a77e467627dc996774f1c5
ankit-chy <ankitkchy101226@gmail.com>
2026-08-11 22:08:19 +0530

    added fixture and fat operator

--- Playwright/tests/UIBasicstest.spec.js
+++ Playwright/tests/UIBasicstest.spec.js
@@ -1,7 +1,14 @@
 const {test} = require('@playwright/test');
 
-test('First Playwright Test', function()
+// test('First Playwright Test', async function()
+// {
+//     // Playwright UI or API Code
+
+// })
+
+test('First Playwright Test', async ({browser}) => // fat operator function example and {browser} is global fixture provided by playwright test runner
 {
     // Playwright UI or API Code
+
 })
  
