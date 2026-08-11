ae910bbf9247b1a58ae63776883ffeb91c9d5497
ankit-chy <ankitkchy101226@gmail.com>
2026-08-11 22:31:16 +0530

    browser context and page fixture

--- Playwright/tests/UIBasicstest.spec.js
+++ Playwright/tests/UIBasicstest.spec.js
@@ -10,5 +10,17 @@ test('First Playwright Test', async ({browser}) => // fat operator function exam
 {
     // Playwright UI or API Code
 
+    const context = await browser.newContext(); // create new browser context
+    const page = await context.newPage(); // create new page in the context
+
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/'); // navigate to the URL
+
 })
  
+test('Page fixture Playwright Test', async ({browser, page}) => // fat operator function example and {browser} is global fixture provided by playwright test runner
+{
+    // Playwright UI or API Code
+    // no need to create new context and page as page is already provided by playwright test runner
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/'); // navigate to the URL
+
+})
